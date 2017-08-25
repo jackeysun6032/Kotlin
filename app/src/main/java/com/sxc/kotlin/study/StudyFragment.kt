14ef43d7bfca534a3496b03a988bd359515c5079
jackeysun6032 <1239512387@qq.com>
2017-08-26 00:22:48 +0800

    Kotlin学习

--- app/src/main/java/com/sxc/kotlin/study/StudyFragment.kt
+++ app/src/main/java/com/sxc/kotlin/study/StudyFragment.kt
@@ -1,6 +1,5 @@
 package com.sxc.kotlin.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -11,7 +10,7 @@ import com.sxc.kotlin.R
 import com.sxc.kotlin.study.KotlinActivity
 import com.sxc.kotlin.study.StudyAdapter
 import kotlinx.android.synthetic.main.fragment_study.*
-import java.util.ArrayList
+import java.util.*
 
 /**
  * Created by sunxunchao on 2017/8/24.
@@ -41,6 +40,6 @@ class StudyFragment : Fragment(), AdapterView.OnItemClickListener {
 
 
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        KotlinActivity.startActivity(activity,studyContent[position])
+        KotlinActivity.startActivity(activity, studyContent[position])
     }
 }
