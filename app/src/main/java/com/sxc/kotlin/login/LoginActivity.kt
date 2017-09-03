b3a1d0ab66ee7778ba2a0bd075ddb72802c28d5f
jackeysun6032 <1239512387@qq.com>
2017-09-03 14:35:34 +0800

    登录界面

--- app/src/main/java/com/sxc/kotlin/login/LoginActivity.kt
+++ app/src/main/java/com/sxc/kotlin/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.sxc.kotlin.login
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import com.sxc.kotlin.MainActivity
 import com.sxc.kotlin.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -12,9 +13,8 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)  //设置全屏
         setContentView(R.layout.activity_login)
-
-        toolbar.title = "Login From"
 
         email_sign_in_button.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
