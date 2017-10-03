bca6d4fef5068e080ef8b1434c57f8ad422c51a0
jackeysun6032 <1239512387@qq.com>
2017-10-03 17:11:14 +0800

    增加地图显示搜索功能 增加历史搜索列表

--- app/src/main/java/com/sxc/kotlin/map/MapActivity.kt
+++ app/src/main/java/com/sxc/kotlin/map/MapActivity.kt
@@ -1,16 +1,18 @@
 package com.sxc.kotlin.map
 
+import android.app.Activity
 import android.content.Intent
 import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import com.amap.api.location.AMapLocation
-import com.amap.api.location.AMapLocationListener
 import com.amap.api.maps.AMap
+import com.amap.api.maps.CameraUpdate
 import com.amap.api.maps.CameraUpdateFactory
 import com.amap.api.maps.UiSettings
-import com.amap.api.maps.model.MyLocationStyle
+import com.amap.api.maps.model.*
+import com.amap.api.services.help.Tip
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.sxc.kotlin.R
 import com.sxc.kotlin.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_map.*
@@ -19,12 +21,14 @@ import kotlinx.android.synthetic.main.activity_map.*
 /**
  * Created by jackey on 2017/9/10.
  */
-class MapActivity : BaseActivity(), AMap.OnMyLocationChangeListener, View.OnClickListener {
+class MapActivity : BaseActivity(), AMap.OnMyLocationChangeListener, View.OnClickListener, AMap.CancelableCallback {
 
     private val TAG: String = MapActivity::class.java.simpleName
 
     companion object {
         val INTERVAL: Long = 3_000L
+        val SEARCH_CONTENT_CODE: Int = 0b010
+        val SEARCH_CONTENT: String = "SEARCH_CONTENT"
     }
 
     private var mUiSettings: UiSettings? = null
@@ -50,6 +54,8 @@ class MapActivity : BaseActivity(), AMap.OnMyLocationChangeListener, View.OnClic
         aMap?.setOnMyLocationChangeListener(this)
 
         search_action.setOnClickListener(this)
+        search_back.setOnClickListener(this)
+        search_clear.setOnClickListener(this)
 
     }
 
@@ -57,7 +63,6 @@ class MapActivity : BaseActivity(), AMap.OnMyLocationChangeListener, View.OnClic
 
         // 如果要设置定位的默认状态，可以在此处进行设置
         myLocationStyle = MyLocationStyle()
-
         myLocationStyle?.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)
         aMap?.myLocationStyle = myLocationStyle
 
@@ -71,11 +76,91 @@ class MapActivity : BaseActivity(), AMap.OnMyLocationChangeListener, View.OnClic
     }
 
     override fun onMyLocationChange(p0: Location?) {
+
         aMap?.animateCamera(CameraUpdateFactory.zoomTo(16f))
+
+    }
+
+    /**
+     * 设置只定位模式
+     */
+    private fun setLocationType(myLocationType: Int) {
+        myLocationStyle = MyLocationStyle()
+        myLocationStyle?.myLocationType(myLocationType)
+        aMap?.myLocationStyle = myLocationStyle
     }
 
     override fun onClick(p0: View?) {
-        startActivity(Intent(this, SearchActivity::class.java))
+        when (p0?.id) {
+            R.id.search_action -> {
+
+                setLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE)
+                aMap?.isMyLocationEnabled = false
+
+                val intent = Intent(this, SearchActivity::class.java)
+                startActivityForResult(intent, SEARCH_CONTENT_CODE)
+            }
+            R.id.search_clear -> {
+                setLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)
+                aMap?.isMyLocationEnabled = true
+                search_clear.visibility = View.GONE
+                search_content.text = "请输入查询信息"
+            }
+
+            R.id.search_back -> finish()
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                SEARCH_CONTENT_CODE -> {
+                    val searchContent: String? = data?.extras?.getString(SEARCH_CONTENT)
+                    val searchBean: Tip? = Gson().fromJson(searchContent, object : TypeToken<Tip>() {}.type)
+                    search_content.text = searchBean?.name
+                    val latLng = LatLng(searchBean?.point?.latitude!!, searchBean?.point?.longitude!!)
+
+                    changeCamera(CameraUpdateFactory.newCameraPosition(CameraPosition(latLng, 18f, 30f, 0f)), this)
+
+                    setMarker(latLng)
+
+                    search_clear.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    /**
+     * 设置marker
+     */
+    private fun setMarker(latLng: LatLng) {
+        aMap?.clear()
+        aMap?.addMarker(MarkerOptions().position(latLng)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
+    }
+
+
+    /**
+     * 根据动画按钮状态，调用函数animateCamera或moveCamera来改变可视区域
+     */
+    private fun changeCamera(update: CameraUpdate, callback: AMap.CancelableCallback?, animated: Boolean = true) {
+        if (animated) {
+            aMap?.animateCamera(update, 1000, callback)
+        } else {
+            aMap?.moveCamera(update)
+        }
+    }
+
+    override fun onFinish() {
+
+    }
+
+    override fun onCancel() {
+
     }
 
     override fun initData() {
--- app/src/main/java/com/sxc/kotlin/map/SearchActivity.kt
+++ app/src/main/java/com/sxc/kotlin/map/SearchActivity.kt
@@ -1,5 +1,6 @@
 package com.sxc.kotlin.map
 
+import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,8 +9,12 @@ import com.amap.api.services.core.AMapException
 import com.amap.api.services.help.Inputtips
 import com.amap.api.services.help.InputtipsQuery
 import com.amap.api.services.help.Tip
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.sxc.kotlin.R
 import com.sxc.kotlin.base.BaseActivity
+import com.sxc.kotlin.map.MapActivity.Companion.SEARCH_CONTENT
+import com.sxc.kotlin.utils.SPUtil
 import com.sxc.kotlin.utils.ToastUtil
 import com.sxc.kotlin.utils.recyclerViewUtil.OnRecyclerViewItemClickListener
 import kotlinx.android.synthetic.main.activity_search.*
@@ -20,6 +25,10 @@ class SearchActivity : BaseActivity(), TextWatcher, Inputtips.InputtipsListener,
     private var searchAdapter: SearchAdapter? = null
     private var mLayoutManager: LinearLayoutManager? = null
 
+    companion object {
+        val HISTORY_SEARCH_LIST: String = "HISTORY_SEARCH_LIST"
+    }
+
     override fun getLayoutId(): Int = R.layout.activity_search
 
     override fun initView() {
@@ -28,6 +37,7 @@ class SearchActivity : BaseActivity(), TextWatcher, Inputtips.InputtipsListener,
         search_list.layoutManager = mLayoutManager
         searchAdapter = SearchAdapter(this)
         search_list.adapter = searchAdapter
+        searchAdapter?.setItems(getHistorySearchList() ?: ArrayList())
 
         city = APPLocation.aMapLocation?.city
         search_content.addTextChangedListener(this)
@@ -48,6 +58,30 @@ class SearchActivity : BaseActivity(), TextWatcher, Inputtips.InputtipsListener,
 
     override fun onItemClick(view: View?, position: Int, data: ArrayList<Tip>) {
         ToastUtil.show(this, data[position].name)
+
+        val selectSearch:String = Gson().toJson(data[position])
+
+        search_content.setText(data[position].name)
+        val intent = Intent()
+        intent.putExtra(SEARCH_CONTENT, selectSearch)
+        setResult(RESULT_OK, intent)
+
+        //保存历史搜索记录
+        val historySearchList: ArrayList<Tip>? = getHistorySearchList() ?: ArrayList()
+        if(!getHistorySearch()?.contains(selectSearch)!!){
+            historySearchList?.add(data[position])
+            SPUtil.put(HISTORY_SEARCH_LIST, Gson().toJson(historySearchList))
+        }
+
+        finish()
+    }
+
+    private fun getHistorySearchList(): ArrayList<Tip>? {
+        return Gson().fromJson<ArrayList<Tip>>(getHistorySearch(), object : TypeToken<ArrayList<Tip>>() {}.type)
+    }
+
+    private fun getHistorySearch(): String?{
+        return SPUtil.get(HISTORY_SEARCH_LIST, "") as String
     }
 
     override fun afterTextChanged(p0: Editable?) {}
